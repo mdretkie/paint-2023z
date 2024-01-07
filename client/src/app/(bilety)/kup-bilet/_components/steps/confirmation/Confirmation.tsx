b077c491f9d253906b3bdeb37c0b021838b864b2
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-01-07 15:10:59 +0100

    Ekran potwierdzenia

--- client/src/app/(bilety)/kup-bilet/_components/steps/confirmation/Confirmation.tsx
+++ client/src/app/(bilety)/kup-bilet/_components/steps/confirmation/Confirmation.tsx
@@ -1,3 +1,49 @@
+import Link from 'next/link';
+
 export default function Confirmation() {
-  return <>Confirmation</>;
+  return (
+    <div className="flex items-center h-full">
+      <div className="flex flex-col items-center gap-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="80"
+          height="80"
+          viewBox="0 0 80 80"
+          fill="none"
+        >
+          <g clipPath="url(#clip0_131_389)">
+            <path
+              d="M40 78C60.9868 78 78 60.9868 78 40C78 19.0132 60.9868 2 40 2C19.0132 2 2 19.0132 2 40C2 60.9868 19.0132 78 40 78Z"
+              stroke="#FB923C"
+              strokeWidth="4"
+            />
+            <path
+              d="M32.8002 48.7751L23.3502 39.3251L20.2002 42.475L32.8002 55.0751L59.8002 28.0751L56.6502 24.925L32.8002 48.7751Z"
+              fill="#FB923C"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_131_389">
+              <rect width="80" height="80" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+        <div className="text-3xl text-zinc-50 font-bold text-center">
+          Bilety Zaklepane! <br />
+          Czas na Filmową Magię!
+        </div>
+        <div className="text-xl text-center text-zinc-50">
+          Dziękujemy za skorzystanie z naszych usług! Proces zakupu biletów
+          zakończony sukcesem. Bilety zostały przesłane na Twój adres e-mail.
+          Życzymy udanego seansu!
+        </div>
+        <Link
+          href={'/'}
+          className="w-fit px-4 py-3 bg-zinc-800 border border-orange-500 rounded-md font-medium text-zinc-50 mb-6 md:mb-0 hover:outline hover:outline-orange-500"
+        >
+          Wróć na stronę główną
+        </Link>
+      </div>
+    </div>
+  );
 }
