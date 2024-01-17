74ef6955ed55cc4117fa61967e0114e980af366e
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-01-17 22:01:00 +0100

    Add log in form

--- client/src/app/(podstawowe)/logowanie/page.tsx
+++ client/src/app/(podstawowe)/logowanie/page.tsx
@@ -24,24 +24,35 @@ export default function Logowanie() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col">
-      <div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center bg-zinc-900 p-16"
+    >
+      <h1 className="text-3xl mb-8 font-bold text-zinc-50">Logowanie</h1>
+      <div className="flex items-center gap-4 relative mb-6">
         <input
+          className={`w-56 text-zinc-50 bg-zinc-800 px-4 py-2 outline outline-zinc-400 rounded-md focus:outline-orange-400 focus:outline-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none `}
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
+          placeholder="Nazwa użytkownika"
         />
       </div>
-      <div>
+      <div className="flex items-center gap-4 relative mb-6">
         <input
+          className="w-56 text-zinc-50 bg-zinc-800 px-4 py-2 outline outline-zinc-400 rounded-md focus:outline-orange-400 focus:outline-2 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          placeholder="Hasło"
         />
       </div>
-      <button type="submit">Login</button>
+      <button
+        className="disabled:opacity-10 disabled:hover:outline-none px-4 py-3 bg-zinc-800 border border-orange-500 rounded-md font-medium text-zinc-50 mb-6 md:mb-0 hover:outline hover:outline-orange-500 w-56"
+        type="submit"
+      >
+        Zaloguj się
+      </button>
     </form>
   );
 }
