7a630b7f180df0aada0cd9bf15a47ace449b2913
maciejkwiatkowski <maciek.kwiatkowski@outlook.com>
2024-01-17 21:44:04 +0100

    Add footer

---
+++ client/src/app/(podstawowe)/logowanie/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useState } from 'react';
+
+export default function Logowanie() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://127.0.0.1:8080/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      localStorage.setItem('token', data.access_token);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <div>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
